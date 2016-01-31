791a321fdbfa84cd10e24b2c068aa0bd43368532
Simon Chauvin <contact@simonchauvin.com>
2016-01-31 01:53:32 +0100

    Missing scripts for player.

---
+++ Assets/Scripts/Player/Profile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Profile : MonoBehaviour
+{
+    private int status;
+
+
+
+	void Start ()
+    {
+        string id = SystemInfo.deviceUniqueIdentifier;
+
+
+    }
+	
+	void Update ()
+    {
+	    
+	}
+}
---
+++ Assets/Scripts/Rituals.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rituals : MonoBehaviour
+{
+    public float minDailySignTime;
+    public float maxDailySignTime;
+
+    private Player player;
+
+
+    void Start ()
+    {
+        player = GameObject.FindObjectOfType<Player>();
+    }
+	
+	void Update ()
+    {
+        int currentHour = int.Parse(string.Format("{0:hh}", System.DateTime.Now));
+        if (currentHour >= minDailySignTime && currentHour <= maxDailySignTime)
+        {
+            if (player.hasDoneSigned())
+            {
+
+            }
+        }
+	}
+}
